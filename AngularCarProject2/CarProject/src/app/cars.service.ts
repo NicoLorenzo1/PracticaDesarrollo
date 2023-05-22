5fe4d426528f7031b9825dc9cd86369b1f657459
[Franco Barlocco] <[titobarlocco52@gmail.com]>
2023-05-22 14:12:40 -0300

    Cheto

--- AngularCarProject2/CarProject/src/app/cars.service.ts
+++ AngularCarProject2/CarProject/src/app/cars.service.ts
@@ -11,32 +11,32 @@ export class CarsService {
   constructor(){
     this.cars = [ {
       imgURL: "https://fotos.perfil.com/2020/01/08/con-el-vision-s-sony-incursiona-en-el-mundo-de-los-autos-853254.jpg",
-      model: "vision-s",
-      description: "electrico",
+      model: "Vision-s",
+      description: "Electrico",
       type: "Auto"
     },
     {
       imgURL: "https://fotos.perfil.com/2020/01/08/con-el-vision-s-sony-incursiona-en-el-mundo-de-los-autos-853254.jpg",
-      model: "vision-s",
-      description: "electrico",
+      model: "Vision-s",
+      description: "Electrico",
       type: "Auto"
     },
     {
       imgURL: "https://fotos.perfil.com/2020/01/08/con-el-vision-s-sony-incursiona-en-el-mundo-de-los-autos-853254.jpg",
-      model: "vision-s",
-      description: "electrico",
+      model: "Vision-s",
+      description: "Electrico",
       type: "Auto"
     },
     {
       imgURL: "https://fotos.perfil.com/2020/01/08/con-el-vision-s-sony-incursiona-en-el-mundo-de-los-autos-853254.jpg",
-      model: "vision-s",
-      description: "electrico",
+      model: "Vision-s",
+      description: "Electrico",
       type: "Auto"
     },
     {
       imgURL: "https://fotos.perfil.com/2020/01/08/con-el-vision-s-sony-incursiona-en-el-mundo-de-los-autos-853254.jpg",
-      model: "vision-s",
-      description: "electrico",
+      model: "Vision-s",
+      description: "Electrico",
       type: "Auto"
     }]
   }
